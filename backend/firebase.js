a89c2be7adc3b34f4bdc5d836be1139a7ac9e9c8
Faith Chulu <chulufaith126@gmail.com>
2024-06-16 02:57:48 +0200

    Service account

--- backend/firebase.js
+++ backend/firebase.js
@@ -1,23 +1,7 @@
-const firebase = require('firebase/app');
-require('firebase/auth');
-const { initializeApp } = require("firebase/app");
-const { getFirestore } = require("firebase/firestore");
+var admin = require("firebase-admin");
 
-const apiKey = process.env.API_KEY;
+var serviceAccount = require("./ServiceAccount/evoting-f6e59-firebase-adminsdk-vuqlr-33629f7ff3.json");
 
-const firebaseConfig = {
-    apiKey: apiKey,
-    authDomain: "evoting-f6e59.firebaseapp.com",
-    projectId: "evoting-f6e59",
-    storageBucket: "evoting-f6e59.appspot.com",
-    messagingSenderId: "160608534616",
-    appId: "1:160608534616:web:3abf6a7580041edd220a2d"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-
-// Get a Firestore instance and assert its type
-const db = getFirestore(app);
-
-module.exports = { db };
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
