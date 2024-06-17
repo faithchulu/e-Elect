5e7275c591358fc20ddcc09af7c073ec695efaeb
Faith Chulu <chulufaith126@gmail.com>
2024-06-17 02:36:04 +0200

    Import error resolve

--- backend/firebase.js
+++ backend/firebase.js
@@ -1,12 +1,9 @@
-import firebase from 'firebase/app';
-import 'firebase/auth';
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-import { getFirestore } from 'firebase/firestore';
+const firebase = require('firebase/app');
+require('firebase/auth');
+const { initializeApp } = require("firebase/app");
+const { getFirestore } = require("firebase/firestore");
 
-
-const apiKey = process.env.API_KEY
-
+const apiKey = process.env.API_KEY;
 
 const firebaseConfig = {
     apiKey: apiKey,
@@ -15,7 +12,7 @@ const firebaseConfig = {
     storageBucket: "evoting-f6e59.appspot.com",
     messagingSenderId: "160608534616",
     appId: "1:160608534616:web:3abf6a7580041edd220a2d"
-  };
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
@@ -23,4 +20,4 @@ const app = initializeApp(firebaseConfig);
 // Get a Firestore instance and assert its type
 const db = getFirestore(app);
 
-export { db };
+module.exports = { db };
