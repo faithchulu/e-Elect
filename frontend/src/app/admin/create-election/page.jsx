bdb33975d4c1eebdb3057b81793523c0bfb21ba7
Faith Chulu <chulufaith126@gmail.com>
2024-06-30 13:49:32 +0200

    Admin screens

---
+++ frontend/src/app/admin/create-election/page.jsx
@@ -0,0 +1,12 @@
+import DefaultLayout from '@/components/Layouts/DefaultLayout'
+import React from 'react'
+
+const CreateElection = () => {
+  return (
+    <DefaultLayout>
+        <div>CreateElection</div>
+    </DefaultLayout>
+  )
+}
+
+export default CreateElection
---
+++ frontend/src/app/admin/historical-elections/page.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import DefaultLayout from '@/components/Layouts/DefaultLayout'
+
+const HistoricalElections = () => {
+  return (
+    <DefaultLayout>
+
+        <div>HistoricalElections</div>
+    </DefaultLayout>
+  )
+}
+
+export default HistoricalElections
