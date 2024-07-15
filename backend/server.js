1b9ce8ba282dc88c59b11b149c58619a266ee2a4
Faith Chulu <chulufaith126@gmail.com>
2024-07-15 11:01:29 +0200

    Server

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 
 const app = express();
-const { db } = require("./firebase");
+const { db } = require("./firebaseAdmin");
 
 // Middleware
 app.use(express.json());
