962d3b4aa220af10ca74d66bcbdd86cc4763079b
Faith Chulu <chulufaith126@gmail.com>
2024-07-24 16:52:40 +0200

    Env Update

--- backend/firebaseAdmin.js
+++ backend/firebaseAdmin.js
@@ -1,9 +1,20 @@
 var admin = require("firebase-admin");
 
-var serviceAccount = require("./serviceAccount/evoting-f6e59-firebase-adminsdk-vuqlr-33629f7ff3.json");
-
+var serviceAccount = {
+  type: process.env.type,
+  project_id: process.env.project_id,
+  private_key_id: process.env.private_key_id,
+  private_key: process.env.private_key,
+  client_email: process.env.client_email,
+  client_id: process.env.client_id,
+  auth_uri: process.env.auth_uri,
+  token_uri: process.env.token_uri,
+  auth_provider_x509_cert_url: process.env.auth_provider_x509_cert_url,
+  client_x509_cert_url: process.env.client_x509_cert_url,
+  universe_domain: process.env.universe_domain,
+};
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+  credential: admin.credential.cert(serviceAccount),
 });
 
 module.exports = admin;
