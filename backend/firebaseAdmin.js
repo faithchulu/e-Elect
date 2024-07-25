8c4524f2d8f651e0317caad23cf3a023f1e469d8
Faith Chulu <chulufaith126@gmail.com>
2024-07-25 08:07:10 +0200

    Firebase environmental variables

--- backend/firebaseAdmin.js
+++ backend/firebaseAdmin.js
@@ -1,10 +1,11 @@
+require("dotenv").config();
 var admin = require("firebase-admin");
 
-var serviceAccount = {
+const serviceAccount = {
   type: process.env.type,
   project_id: process.env.project_id,
   private_key_id: process.env.private_key_id,
-  private_key: process.env.private_key,
+  private_key: process.env.private_key.replace(/\\n/g, "\n"),
   client_email: process.env.client_email,
   client_id: process.env.client_id,
   auth_uri: process.env.auth_uri,
@@ -13,8 +14,11 @@ var serviceAccount = {
   client_x509_cert_url: process.env.client_x509_cert_url,
   universe_domain: process.env.universe_domain,
 };
+
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
 
-module.exports = admin;
+const db = admin.firestore();
+
+module.exports = { admin, db };
