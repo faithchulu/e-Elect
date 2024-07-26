6dc332f725489287c6a55c9db96b09b3b711e4c4
Faith Chulu <chulufaith126@gmail.com>
2024-07-26 08:00:41 +0200

    Home

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,75 +1,80 @@
-import { useState, useEffect } from 'react';
-import Link from 'next/link';
-import { ClockIcon, CheckCircleIcon, ExclamationCircleIcon } from '@heroicons/react/24/outline';
-import Image from 'next/image';
-import HorizontalNav from '@/components/HorizontalNav/HorizontalNav';
-import Logo from '../../public/images/logo/e-Elect-Logo.png';
-import HomeBG from '../../public/images/backgrounds/zambia_flag.png';
+"use client";
+import { useState, useEffect } from "react";
+import Link from "next/link";
+import {
+  ClockIcon,
+  CheckCircleIcon,
+  ExclamationCircleIcon,
+} from "@heroicons/react/24/outline";
+import Image from "next/image";
+import axios from "axios";
+import HorizontalNav from "@/components/HorizontalNav/HorizontalNav";
+import Logo from "../../public/images/logo/e-Elect-Logo.png";
+import HomeBG from "../../public/images/backgrounds/zambia_flag.png";
 
 interface Election {
-  id: number;
+  id: string;
   name: string;
-  status: 'registration' | 'voting' | 'closed';
+  status: "registration" | "voting" | "closed";
 }
 
-// Mock data for currently active and historical elections
-const activeElections: Election[] = [
-  { id: 1, name: '2024 Presidential Election', status: 'registration' },
-  { id: 2, name: '2024 Congressional Election', status: 'voting' },
-];
-
-const historicalElections: Election[] = [
-  { id: 3, name: '2023 Local Election', status: 'closed' },
-];
-
 const ElectionCard = ({ election }: { election: Election }) => {
-  const getStatusIcon = (status: Election['status']) => {
+  const getStatusIcon = (status: Election["status"]) => {
     switch (status) {
-      case 'registration':
-        return <ClockIcon className="w-6 h-6 text-orange-500" />;
-      case 'voting':
-        return <CheckCircleIcon className="w-6 h-6 text-green-500" />;
-      case 'closed':
-        return <ExclamationCircleIcon className="w-6 h-6 text-red-500" />;
+      case "registration":
+        return <ClockIcon className="h-6 w-6 text-orange-500" />;
+      case "voting":
+        return <CheckCircleIcon className="h-6 w-6 text-green-500" />;
+      case "closed":
+        return <ExclamationCircleIcon className="text-red-500 h-6 w-6" />;
       default:
         return null;
     }
   };
 
-  const getStatusText = (status: Election['status']) => {
+  const getStatusText = (status: Election["status"]) => {
     switch (status) {
-      case 'registration':
-        return 'Registration Open';
-      case 'voting':
-        return 'Voting in Progress';
-      case 'closed':
-        return 'Election Closed';
+      case "registration":
+        return "Registration Open";
+      case "voting":
+        return "Voting in Progress";
+      case "closed":
+        return "Election Closed";
       default:
         return null;
     }
   };
 
-  const getActionText = (status: Election['status']) => {
+  const getActionText = (status: Election["status"]) => {
     switch (status) {
-      case 'registration':
-        return 'Register to Vote';
-      case 'voting':
-        return 'Cast Your Vote';
-      case 'closed':
-        return 'View Results';
+      case "registration":
+        return "Register to Vote";
+      case "voting":
+        return "Cast Your Vote";
+      case "closed":
+        return "View Results";
       default:
         return null;
     }
   };
 
   return (
-    <div className="p-4 bg-slate-950 shadow-lg rounded-lg">
+    <div className="rounded-lg bg-slate-950 p-4 shadow-lg">
       <div className="flex items-center justify-between">
         <h2 className="text-lg font-semibold text-white">{election.name}</h2>
         {getStatusIcon(election.status)}
       </div>
-      <p className="mt-2 text-gray-300">{getStatusText(election.status)}</p>
-      <Link href={election.status === 'registration' ? '/voter-registration/2345' : election.status === 'voting' ? '/cast-vote/37848dhhd8' : '/results/37848dhhd8'} className="mt-4 block w-full text-center bg-green-600 text-white py-2 rounded-md hover:bg-green-500">
+      <p className="text-gray-300 mt-2">{getStatusText(election.status)}</p>
+      <Link
+        href={
+          election.status === "registration"
+            ? "/voter-registration/2345"
+            : election.status === "voting"
+              ? "/cast-vote/37848dhhd8"
+              : "/results/37848dhhd8"
+        }
+        className="mt-4 block w-full rounded-md bg-green-600 py-2 text-center text-white hover:bg-green-500"
+      >
         {getActionText(election.status)}
       </Link>
     </div>
@@ -77,6 +82,39 @@ const ElectionCard = ({ election }: { election: Election }) => {
 };
 
 const LandingPage = () => {
+  const [elections, setElections] = useState<Election[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchElections = async () => {
+      try {
+        const response = await axios.get(
+          "https://e-elect-backend.vercel.app/api/election/get-elections",
+        );
+        setElections(response.data);
+        console.log(elections);
+        setLoading(false);
+      } catch (error) {
+        setError("Failed to fetch elections");
+        setLoading(false);
+      }
+    };
+
+    fetchElections();
+  }, []);
+
+
+  if (error) return <div>{error}</div>;
+
+  const activeElections = elections.filter(
+    (election) =>
+      election.status === "registration" || election.status === "voting",
+  );
+  const historicalElections = elections.filter(
+    (election) => election.status === "closed",
+  );
+
   return (
     <div className="relative min-h-screen">
       <div className="absolute inset-0">
@@ -92,14 +130,18 @@ const LandingPage = () => {
       <div className="relative z-10 min-h-screen py-24">
         <HorizontalNav />
         <div className="p-4">
-          <h2 className="text-xl font-semibold mb-4 text-white">Currently Active Elections</h2>
-          <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+          <h2 className="mb-4 text-xl font-semibold text-white">
+            Currently Active Elections
+          </h2>
+          <div className="grid grid-cols-1 gap-4 md:grid-cols-3">
             {activeElections.map((election) => (
               <ElectionCard key={election.id} election={election} />
             ))}
           </div>
-          <h2 className="text-xl font-semibold mt-8 mb-4 text-white">Historical Elections</h2>
-          <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+          <h2 className="mb-4 mt-8 text-xl font-semibold text-white">
+            Historical Elections
+          </h2>
+          <div className="grid grid-cols-1 gap-4 md:grid-cols-3">
             {historicalElections.map((election) => (
               <ElectionCard key={election.id} election={election} />
             ))}
