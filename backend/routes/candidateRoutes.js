808cd5b025bc32b888ee1bcb7335c8b96a21fb8b
Faith Chulu <chulufaith126@gmail.com>
2024-08-08 13:27:19 +0200

    up

--- backend/routes/candidateRoutes.js
+++ backend/routes/candidateRoutes.js
@@ -6,6 +6,7 @@ const router = express.Router();
 router.get("/api/election/get-candidates/:electionId", async (req, res) => {
   const { electionId } = req.params;
   const result = await getCandidatesByElectionId(electionId);
+  
   if (result.success) {
     res.status(200).json(result.candidates);
   } else {
