3be3f14aba5352f042c6fff98533b1b367af0796
Faith Chulu <chulufaith126@gmail.com>
2024-11-22 02:08:02 +0200

    CORS update 2

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin:  ['http://localhost:3000', 'https://e-elect.vercel.app/'],
+    origin:  true,
     allowedHeaders: [
       "Access-Control-Allow-Origin",
       "Content-Type",
