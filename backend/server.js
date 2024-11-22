55bffa09748ddeca974bfd8a12176a1c0872316f
Faith Chulu <chulufaith126@gmail.com>
2024-11-22 02:16:09 +0200

    Backend cors test

--- backend/server.js
+++ backend/server.js
@@ -13,14 +13,7 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin:  ['http://localhost:3000', 'https://e-elect.vercel.app/'],
-    allowedHeaders: [
-      "Content-Type",
-      "Authorization",
-    ],
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    preflightContinue: false,
-    credentials: true,
+    origin:  ['http://localhost:3000', 'https://e-elect.vercel.app/']
   })
 );
 
