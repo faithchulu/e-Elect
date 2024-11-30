223a24b933a548edbe8a4927a2ca8b08a4b9075f
Faith Chulu <chulufaith126@gmail.com>
2024-11-30 16:43:33 +0200

    updated ip addresses

--- frontend/src/app/api.ts
+++ frontend/src/app/api.ts
@@ -1,5 +1,5 @@
-export const API1: string = "https://192.168.56.1:4000";
+export const API1: string = "https://192.168.138.241:4000";
 
 //export const API1:string="https://e-elect-fingerprint-backend.vercel.app"
 
-export const API2: string = "https://192.168.56.1:5000";
+export const API2: string = "https://192.168.138.241:5000";
