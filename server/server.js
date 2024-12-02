0e70db5a54b8fb2aee65eb21151eea2d2a9b2822
Faith Chulu <chulufaith126@gmail.com>
2024-12-02 21:30:22 +0200

    kjnm

--- server/server.js
+++ server/server.js
@@ -13,7 +13,6 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://e-elect.vercel.app"],
     allowedHeaders: [
       "Access-Control-Allow-Origin",
       "Content-Type",
