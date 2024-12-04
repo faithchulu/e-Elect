e666eee56e2186854416fa23279d86ac944e9ae8
Faith Chulu <chulufaith126@gmail.com>
2024-12-05 01:16:40 +0200

    Main server

--- backend/controller/scanController.js
+++ backend/controller/scanController.js
@@ -10,11 +10,11 @@ const {
   updateVoterCounter,
 } = require("../services/scanService");
 
-// const CLIENT_URL = "https://e-elect.vercel.app";
-// const RP_ID = "e-elect.vercel.app";
+const CLIENT_URL = "https://e-elect.vercel.app";
+const RP_ID = "e-elect.vercel.app";
 
-const CLIENT_URL = "http://localhost:3000";
-const RP_ID = "localhost";
+// const CLIENT_URL = "http://localhost:3000";
+// const RP_ID = "localhost";
 
 const initRegister = async (req, res) => {
   const nrcNumber = req.query.nrcNumber;
